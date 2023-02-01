73868a9a09a70efe53ee4841e6c6cacbacc66ad5
Giorgi Martiashvili <Giorgi.Martiashvili@space.ge>
2023-02-01 14:56:01 +0400

    remove namespaces

--- Qinchili.API/Controllers/CustomerController.cs
+++ Qinchili.API/Controllers/CustomerController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Modules.Customers;
+﻿using Microsoft.AspNetCore.Mvc;
 using Modules.Customers;
 
 namespace Qinchili.API.Controllers
